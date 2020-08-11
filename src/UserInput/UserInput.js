3e122ebcf2327cef0b160693054854d951a917aa
Chandan Kumar <descentchandan@gmail.com>
2020-08-11 22:23:55 +0530

    used inline styling

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,8 +1,14 @@
 import React from "react"
 
 const userInput=(props)=>{
+    const style={
+        border:"2px solid red",
+        width:"200px",
+        margin:"20px auto"
 
-    return <input type="text" onChange={props.change} value={props.curName} />
+    }
+
+    return <input style={style} type="text" onChange={props.change} value={props.curName} />
 }
 
 export default userInput
