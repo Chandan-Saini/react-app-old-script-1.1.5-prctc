3017fe74a4e51c20a290c1dcf87e055f6e5efac3
Chandan Kumar <descentchandan@gmail.com>
2020-08-11 21:47:21 +0530

    added state in container comp class

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import UserInput from "./UserInput/UserInput"
 import UserOutput from "./UserOutput/UserOutput"
 
 class App extends Component {
+
+  state={
+    userName:" Chandan from state"
+  }
   render() {
+
     return (
       <div className="App">
         
         <UserInput />
-        <UserOutput userName={"chandan"} />
-        <UserOutput userName={"aman"} />
+        <UserOutput userName={this.state.userName} />
+        <UserOutput userName={this.state.userName} />
         <UserOutput userName={"akku"} />
       </div>
     );
