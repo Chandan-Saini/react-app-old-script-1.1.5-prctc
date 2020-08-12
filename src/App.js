a8b44c27356e8d188beb56b194d134d63436c2e6
Chandan Kumar <descentchandan@gmail.com>
2020-08-12 17:34:31 +0530

    started assign 2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import UserInput from "./UserInput/UserInput"
-import UserOutput from "./UserOutput/UserOutput"
+
 
 class App extends Component {
 
-  state={
-    userName:" Chandan from state"
-  }
-
-  stateHandler=(event)=>{
-    this.setState({
-      userName:event.target.value
-    })
-  }
+ 
   render() {
 
     return (
-      <div className="App">
-        
-        <UserInput change={this.stateHandler} curName={this.state.userName} />
-        <UserOutput click={this.stateHandler} userName={this.state.userName} />
-        <UserOutput userName={this.state.userName} />
-        <UserOutput userName={"akku"} />
-      </div>
+      <input change={} />
     );
   }
 }
