ca69c10c1891e75048e9ec829dc08f3daf5ea48a
Chandan Kumar <descentchandan@gmail.com>
2020-08-13 13:31:10 +0530

    try to make charcomp

--- src/CharComp/CharComp.js
+++ src/CharComp/CharComp.js
@@ -1,10 +1,16 @@
 import React from "react"
 
 const charComp=(props)=>{
+   style={
+       display:"inline-block",
+       border:"2px solid black",
+       width:"40px",
+       height:"40px",
+       textAlign:"center",
+       padding:"5px"
+   }
 
-    return (
-    
-    )
+    return <p style={style}>{props.strng} </p>
 }
 
 export default charComp
