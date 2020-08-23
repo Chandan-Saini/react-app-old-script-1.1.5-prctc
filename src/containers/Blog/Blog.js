34564a8387bda4055a4e8e1ff1a7486442a93eac
Chandan Kumar <descentchandan@gmail.com>
2020-08-24 00:49:47 +0530

    rendering comp for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -18,8 +18,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={()=> <h1>Home route</h1>} />
-                <Route path="/" exact render={()=> <h1>Home route 2</h1>} />
+                {/* <Route path="/" exact render={()=> <h1>Home route</h1>} />
+                <Route path="/" exact render={()=> <h1>Home route 2</h1>} /> */}
+
+                <Route route='/' exact component={Posts}  />
             </div>
         );
     }
