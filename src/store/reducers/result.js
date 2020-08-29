24cfd6ef7c69d9bacf56e376ed345ca3f6a043db
Chandan Kumar <descentchandan@gmail.com>
2020-08-30 00:29:27 +0530

    used utility to shorten the obj in reducers

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from "../utility";
 
 const initialState = {
     results: []
@@ -7,19 +8,22 @@ const initialState = {
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result})  //way to shallow copy arrays
-            }
+            return updateObject({
+              state,
+              results: state.results.concat({
+                id: new Date(),
+                value: action.result,
+              })
+            });
         case actionTypes.DELETE_RESULT:
             // const id = 2;
             // const newArray = [...state.results];       //way 2 to shallow copy arrays
             // newArray.splice(id, 1)                    //way to delete the element from array
             const updatedArray = state.results.filter(result => result.id !== action.resultElId); //way to delete element from array without effecting original array, as filter returns new array when condition satisfies 
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return updateObject({
+              state,
+              results: updatedArray,
+            });
     }
     return state;
 };
